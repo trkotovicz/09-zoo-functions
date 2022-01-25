31bf28defc3a0c30e6a38e9770b07e3afa361639
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-25 19:17:05 -0300

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  // Se o parâmetro for indefinido, retorna array vazio.
+  if (ids === undefined) {
+    return [];
+  }
+  // Caso contrário, filtra (.filter()) o array 'species' e procura (.find()) o elemento com o id informado. Retorna o elemento inteiro (objeto).
+  return data.species.filter((specie) => ids.find((id) => (specie.id === id)));
+  // O spread (...) serve para caso de informarmos mais de um id, retornar todos juntos em um único array.
 }
 
 module.exports = getSpeciesByIds;
