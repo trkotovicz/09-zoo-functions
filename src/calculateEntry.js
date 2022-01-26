6a0a3d2cee6e210af530b5c1211431b1e39a1f18
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-26 15:31:51 -0300

    Requisito 5 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  //  seu código aqui
+  entrants.reduce((accumulator, currentValue) => {
+    if (currentValue.age < 18) {
+      accumulator.child += 1;
+    } else if (currentValue.age < 50) {
+      accumulator.adult += 1;
+    } else {
+      accumulator.senior += 1;
+    }
+    return accumulator;
+  // aqui eu coloco o parâmetro para o acumulador começar sendo esse objeto com as propriedades que preciso. Cada vez que o currentValue entrar em uma condição de idade, ele será adicionado na propriedade respectiva.
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const countedEntrants = countEntrants(entrants);
+  return ((countedEntrants.child * data.prices.child)
+  + (countedEntrants.adult * data.prices.adult)
+  + (countedEntrants.senior * data.prices.senior));
 }
+
+/*
+Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+Object.keys() retorna um array cujo os  elementos são strings correspondentes para a propriedade enumerável encontrada diretamento sobre o objeto. A ordenação das propriedades é a mesma que a dada pelo loop sobre as propriedades do objeto manualmente.
+Ou seja, ele vai acessar todas as chaves (propriedades) do objeto e, se o objeto tiver o 'length' igual a 0, será um objeto vazio.
+*/
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // Se receber um parâmetro vazio, deve retornar um objeto com todas as espécies e a quantidade de residentes de cada uma. O forEach varre o array das especieis e pega os parâmetros 'nome' e 'residentes' de todos os animais e salva na variável de objeto.
+  if (animal === undefined) {
+    const retornaObjeto = {};
+    data.species.forEach((animais) => { retornaObjeto[animais.name] = animais.residents.length; });
+    // atribui uma propriedade ao objeto com seu respectivo valor, no caso a propriedade é o animal e o valor é o número de residentes
+    return retornaObjeto;
+  }
+
+  const findSpecie = data.species.find((especie) => especie.name === animal.specie);
+  // encontra a espécie informada e se a propriedade sexo não for informada, retorna o número de residentes daquela espécie
+  if (animal.sex === undefined) {
+    return findSpecie.residents.length;
+  }
+  // caso seja informado o sexo do  animal, ele filtra dentro dos residentes encontrados os animais daquele sexo e retorna o número
+  return findSpecie.residents.filter((residente) => residente.sex === animal.sex).length;
 }
+
+console.log(countAnimals({ specie: 'giraffes' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
+
+// return data.species.map((animais) => `${animais.name}: ${animais.residents.length}`);
+// está retornando um array, preciso que retorne um objeto
 
 module.exports = countAnimals;
