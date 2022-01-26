ebe8e8258d34ca24f04515d0ff902dedeea5047f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-25 22:19:48 -0300

    Requisito 2 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Função que busca (.find()) o animal informado com a propriedade name igual.
+  return data.species.find((element) => element.name === animal)
+    .residents.every((animaisVelhos) => animaisVelhos.age >= age);
+  // Depois pega todo (.every()) animal que tenha a idade passada ou mais.
 }
 
 module.exports = getAnimalsOlderThan;
